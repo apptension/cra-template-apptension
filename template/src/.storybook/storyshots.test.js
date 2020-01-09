da2c7b7e98bcbb0df152248a5b2b53ca6e66bb24
Michał Przyszczypkowski <mprzyszczypkowski@apptension.com>
2020-01-09 11:27:57 +0100

    Use testing-library serializer in snapshots

--- template/src/.storybook/storyshots.test.js
+++ template/src/.storybook/storyshots.test.js
@@ -1,12 +1,21 @@
 import initStoryshots, { multiSnapshotWithOptions, Stories2SnapsConverter } from '@storybook/addon-storyshots';
-import toJSON from 'enzyme-to-json';
+import { render } from "@testing-library/react";
+import React from 'react';
+
+const reactTestingLibrarySerializer = {
+  print: (val, serialize, indent) => serialize(val.container),
+  test: (val) => val && val.hasOwnProperty("container")
+};
 
 initStoryshots({
   configPath: './src/.storybook',
   framework: 'react',
   shallowSnapshot: true,
-  serializer: toJSON,
-  test: multiSnapshotWithOptions(),
+  snapshotSerializers: [reactTestingLibrarySerializer],
+  test: multiSnapshotWithOptions({
+    renderer: render
+  }),
+
   stories2snapsConverter: new Stories2SnapsConverter({
     snapshotsDirName: './__tests__/__snapshots__',
   }),
