9355913ca7aaf2fd66762e1d52f6ce7763862f8b
Jakub Wiśniewski <jwisniewski@apptension.com>
2020-01-13 11:08:45 +0100

    add typescript

--- template/src/routes/home/index.js
+++ template/src/routes/home/index.js
@@ -1,4 +1,4 @@
 import { hot } from 'react-hot-loader/root';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './home.component.tsx';
 
 export const Home = hot(HomeComponent);
