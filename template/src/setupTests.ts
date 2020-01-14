5dd944f987c9e737bb99192eef500aa71752d817
Michał Przyszczypkowski <mprzyszczypkowski@apptension.com>
2020-01-14 13:32:57 +0100

    Change setupTests to TS

--- template/src/setupTests.ts
+++ template/src/setupTests.ts
@@ -14,12 +14,15 @@ afterEach(() => {
   nock.cleanAll();
 });
 
+// @ts-ignore
 window.matchMedia =
   window.matchMedia ||
   function() {
     return {
       matches: false,
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
       addListener: function() {},
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
       removeListener: function() {},
     };
   };
