4e021ad963ddcf6dbc2635c2cd76dd38eee959e0
Jakub Wiśniewski <jwisniewski@apptension.com>
2020-01-16 11:27:53 +0100

    remove unused import

--- template/src/modules/sagas.ts
+++ template/src/modules/sagas.ts
@@ -1,6 +1,5 @@
 import { all, fork } from 'redux-saga/effects';
 
-import reportError from '../shared/utils/reportError';
 import { watchStartup } from './startup/startup.sagas';
 import { watchUsers } from './users/users.sagas';
 //<-- IMPORT MODULE SAGA -->
