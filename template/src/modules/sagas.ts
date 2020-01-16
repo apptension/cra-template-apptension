3ebeeaf9ebc44c85995b9e52d60dba0ff6341cb8
Jakub Wiśniewski <jwisniewski@apptension.com>
2020-01-16 11:26:08 +0100

    remove try catch from sagas root

--- template/src/modules/sagas.ts
+++ template/src/modules/sagas.ts
@@ -6,13 +6,9 @@ import { watchUsers } from './users/users.sagas';
 //<-- IMPORT MODULE SAGA -->
 
 export default function* rootSaga() {
-  try {
-    yield all([
-      fork(watchStartup),
-      fork(watchUsers),
-      //<-- INJECT MODULE SAGA -->
-    ]);
-  } catch (e) {
-    yield reportError(e);
-  }
+  yield all([
+    fork(watchStartup),
+    fork(watchUsers),
+    //<-- INJECT MODULE SAGA -->
+  ]);
 }
