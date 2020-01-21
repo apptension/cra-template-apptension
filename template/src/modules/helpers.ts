81c2e9b4109cd047c7293f67326e128c338baa98
Michał Przyszczypkowski <mikeqcp@users.noreply.github.com>
2020-01-21 09:25:29 +0100

    Use immer implicitly in reducers (#30)

--- template/src/modules/helpers.ts
+++ template/src/modules/helpers.ts
@@ -1,21 +1,12 @@
-import produce from 'immer';
-import { map } from 'ramda';
-import { createAction, createReducer, PayloadActionCreator } from '@reduxjs/toolkit';
+import { createAction, PayloadActionCreator } from '@reduxjs/toolkit';
 
 export interface ReduxAction<T> {
   type: string;
   payload: T;
 }
 
-export type Reducer<T> = (currentState: T, action: ReduxAction<any>) => void;
-
 export const actionCreator = (prefix: string) => <T>(actionName: string) =>
   createAction<T>([prefix, actionName].join('/'));
-
-export const createImmutableReducer = <T>(initialState: T, reducers: { [key: string]: Reducer<T> }) => {
-  // @ts-ignore
-  return createReducer(initialState, map(produce, reducers));
-};
 
 export const actionHandler = <P, S>(
   action: PayloadActionCreator<P>,
