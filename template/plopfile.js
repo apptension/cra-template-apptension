b3c49a17a71cbe9b5cbe96f84c52f785e7a191ad
Michał Przyszczypkowski <mprzyszczypkowski@apptension.com>
2021-01-15 10:29:41 +0100

    Add api model plop generator

--- template/plopfile.js
+++ template/plopfile.js
@@ -3,11 +3,13 @@ const promptDirectory = require('inquirer-directory');
 const addReduxModuleGenerator = require('./plop/reduxModule');
 const addReactComponentGenerator = require('./plop/reactComponent');
 const addReactHookGenerator = require('./plop/reactHook');
+const addApiModelGenerator = require('./plop/apiModel');
 
-module.exports = function(plop) {
+module.exports = function (plop) {
   plop.setPrompt('directory', promptDirectory);
 
   addReduxModuleGenerator(plop);
   addReactComponentGenerator(plop);
   addReactHookGenerator(plop);
+  addApiModelGenerator(plop);
 };
